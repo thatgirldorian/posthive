126f6473e0632aefb33574fe0d94a29076c08253
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-05-06 19:59:08 +0400

    feat: Added a way to show posts on homepage

---
+++ components/Post.js
@@ -0,0 +1,3 @@
+export default function Post({ post }) {
+  return <p>{post.content}</p>;
+}
--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,20 @@
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import NewPost from "components/NewPost";
+import Posts from "components/Posts";
 
 export default function Home() {
   const { data: session, status } = useSession();
+  const loading = status === "loading";
+  const router = useRouter();
+
+  if (loading) {
+    return null;
+  }
+
+  if (!session) {
+    router.push("/");
+  }
 
   if (status === "loading") {
     return <p>Loading...</p>;
@@ -10,9 +22,15 @@ export default function Home() {
 
   return (
     <div
-      className={`flex  bg-white min-h-screen flex-col items-center justify-between p-24 `}
+      className={`bg-white min-h-screen flex-col items-center justify-between p-24 `}
     >
-      {session ? <NewPost /> : <p>Login failed. Please try again. 🙁</p>}
+      <NewPost />
+      <Posts
+        posts={[
+          { content: "MY first post!" },
+          { content: "another new post :)" },
+        ]}
+      />
     </div>
   );
 }
