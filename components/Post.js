6ccda3a347423436a88ea320d34a89cdcd69b465
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-05-07 20:36:07 +0400

    feat: changed up the post dates

--- components/Post.js
+++ components/Post.js
@@ -1,3 +1,10 @@
+import timeago from "lib/timeago";
+
 export default function Post({ post }) {
-  return <p>{post.content}</p>;
+  return (
+    <p>
+      {post.content}
+      {timeago.format(new Date(post.createdAt))}
+    </p>
+  );
 }
