0ead3c840868765ebe2077f03f1475affccd308c
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-05-09 13:06:29 +0400

    feat: show post images

--- components/Post.js
+++ components/Post.js
@@ -1,8 +1,17 @@
 import timeAgo from "lib/timeago";
+import Image from "next/image";
 
 export default function Post({ post }) {
   return (
     <p>
+      {post.author.image && (
+        <Image
+          src={post.author.image}
+          alt={post.author.name}
+          width={40}
+          height={40}
+        />
+      )}
       {timeAgo.format(new Date(post.createdAt))} {post.author.name}{" "}
       {post.content}
     </p>
